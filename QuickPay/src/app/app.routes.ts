189fae943fe8cb9eecff72781ad83d612dbc220a
Luis <luiseduardo.fdez@alu.uclm.es>
2025-02-28 12:39:36 +0100

    Inicio Login

---
+++ QuickPay/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // Optional: Default route
+];
